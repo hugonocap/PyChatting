c4ba782c5217a8e2914d8ac898356647ee17f66e
17seannnn <17sean@protonmail.com>
2022-06-03 19:19:35 +0300

    refactor: ease partition_quotes()

--- common.py
+++ common.py
@@ -2,7 +2,6 @@ def partition_quotes(str):
     if (len(str) < 2) or (str[0] not in ['\'', '"']):
         return (str, '')
 
-    # Try to find quote from 1 position
     quote = str[0]
     start = 1
     while True:
@@ -14,10 +13,4 @@ def partition_quotes(str):
         else:
             break
 
-    # Try to get the rest of the string
-    if quote_pos+2 < len(str):
-        other = str[quote_pos+2:]
-    else:
-        other = ''
-
-    return (str[1:quote_pos].replace('\\', ''), other)
+    return (str[1:quote_pos].replace('\\', ''), str[quote_pos+2:])
